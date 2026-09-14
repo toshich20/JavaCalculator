854eea6291f766f4bd775570b2c0f88fa481feca
toshich20 <toshich20@gmail.com>
2026-09-14 17:10:48 +0300

    feat: add getOperand method with input validation

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static double getOperand(Scanner scanner) {
+        while (true) {
+            System.out.print("Введите операнд: ");
+            if (scanner.hasNextDouble()) {
+                double operand = scanner.nextDouble();
+                return operand;
+            } else {
+                System.out.println("Операнд должен быть числом!");
+                scanner.next();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double firstOperand = getOperand(scanner);
+    }
 }
