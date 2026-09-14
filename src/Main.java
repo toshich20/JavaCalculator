a18845971a6ce5383c45d38d2ad55b20cc6cd813
toshich20 <toshich20@gmail.com>
2026-09-14 20:55:26 +0300

    feat: implement continuous calculation loop with reset and exit commands

--- src/Main.java
+++ src/Main.java
@@ -17,11 +17,11 @@ public class Main {
 
     private static char getOperation(Scanner scanner) {
         while (true) {
-            System.out.print("Введите операцию [+|-|*|/]: ");
-            String input = scanner.next();
+            System.out.print("Введите операцию (+, -, *, /, c - сброс, s - выход): ");
+            String input = scanner.next().toLowerCase();
             if (input.length() == 1) {
                 char operation = input.charAt(0);
-                if (operation == '+' || operation == '-' || operation == '*' || operation == '/') {
+                if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == 'c' || operation == 's') {
                     return operation;
                 }
             }
@@ -44,21 +44,37 @@ public class Main {
         }
     }
 
-    public static boolean isZeroDivision(char operation, double secondOperand) {
+    private static boolean isZeroDivision(char operation, double secondOperand) {
         return operation == '/' && secondOperand == 0;
     }
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         double firstOperand = getOperand(scanner);
-        char operation = getOperation(scanner);
-        double secondOperand = getOperand(scanner);
-        while (isZeroDivision(operation, secondOperand)) {
-            System.out.print("На ноль делить нельзя! Измените второй операнд: ");
-            secondOperand = getOperand(scanner);
+
+        while (true) {
+            char operation = getOperation(scanner);
+
+            switch (operation) {
+                case 'c':
+                    System.out.println("Результат сброшен.");
+                    firstOperand = getOperand(scanner);
+                    continue;
+                case 's':
+                    System.out.println("Работа калькулятора завершена.");
+                    return;
+                default:
+                    double secondOperand = getOperand(scanner);
+                    while (isZeroDivision(operation, secondOperand)) {
+                        System.out.println("На ноль делить нельзя! Измените второй операнд.");
+                        secondOperand = getOperand(scanner);
+                    }
+                    double result = calculate(firstOperand, operation, secondOperand);
+                    System.out.println(result);
+                    firstOperand = result;
+            }
         }
-        double result = calculate(firstOperand, operation, secondOperand);
-        System.out.println(result);
     }
 }
